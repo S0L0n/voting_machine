b4e18eef1166ab98e5b446ae8371b4ed14263c69
Emanuel Matheus <emanuelmatheusssm@hotmail.com>
2015-11-04 23:09:04 -0200

    first commit on voting_machine
    adding the principal files.

---
+++ corrigir.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once 'urna_eletronica.class.php';
+
+$corrigir = new Voto('');
+$corrigir->corrigirVoto();
+
---
+++ urna_eletronica.class.php
@@ -0,0 +1,39 @@
+<?php
+
+class Voto{
+    
+    public $_Nvoto;
+    public $_arquivo;
+    
+    function __construct($voto) {
+        $this->_Nvoto = $voto;
+        $this->_arquivo = "votos.txt";
+    }
+    public function votar($voto){
+        $arquivo = $this->_arquivo;
+
+        $conteudo = $voto."\r\n";
+echo $voto;
+        $abertura = fopen("$arquivo","a+");
+        $gravacao = fwrite($abertura,$conteudo);
+        fclose($abertura);
+        $_SESSION['codigo'] = 0;
+        header("location:index.php?return=1");
+        
+    }
+    
+    public function contaVoto($cod,$codigos){
+        session_start();
+              $_SESSION['codigo'] = $codigos.$cod;
+              header("location:index.php");
+
+    }
+    public function corrigirVoto(){
+        session_start();
+        $_SESSION['codigo'] = '';
+       
+        header("location:index.php");
+        
+    }
+    
+}
